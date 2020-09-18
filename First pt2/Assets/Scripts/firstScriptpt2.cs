611d177e91984d682cfdc98af9937f6ef1ca583a
FSD38\kadyn.chinn <kadyn.chinn@gm.fsd38.ab.ca>
2020-09-18 11:11:50 -0600

    2nd week

---
+++ First pt2/Assets/Scripts/firstScriptpt2.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstScriptpt2 : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    SpriteRenderer spriteRen;
+    Animator tankAnim;
+
+    // declare variables
+    // we need a rigidbody refrence
+    // we need a sprite renderer reference
+    // and we need an animator reference
+
+    // Use this for initialization
+    void Start ()
+    {
+        Debug.Log("Hello World");
+
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRen = GetComponent<SpriteRenderer>();
+        tankAnim = GetComponent<Animator>();
+
+        
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // get user input
+
+        // if user pushes up key, then move gameobject up
+        // check for key pressed
+        // change rigidbody velocity (0, 2)
+        // play animation: Player Move 1
+
+        // if user pushes the down key, move gameobject down
+        // get user input
+        // if user pushes up key, then move gameobject up
+        // check for key pressed
+        // change rigidbody velocity( 0, -2)
+        // play animation: Player Move 1
+        // flip sprite on y
+
+        // if user pushes right, move right
+        // get user input
+        // if user pushes up key, then move gameobject up
+        // check for key pressed
+        // change rigidbody velocity (2, 0)
+        // play animation: PlayerMoveRight
+
+        // if user pushes left, move left
+        // get user input
+        // if user pushes up key, then move gameobject up
+        // check for key pressed
+        // change rigidbody velocity (-2, 0)
+        // flip sprite on x
+        // play animation: PlayerMoveRight
+
+        // Deal with user input
+        if (Input.GetKey("w") || Input.GetKey("up"))
+        {
+            rb2d.velocity = new Vector2(0, 2);
+            spriteRen.flipY = false;
+            tankAnim.Play("Player Move 1");
+        }
+        else if (Input.GetKey("s") || Input.GetKey("down"))
+        {
+            rb2d.velocity = new Vector2(0, -2);
+            spriteRen.flipY = true;
+            tankAnim.Play("Player Move 1");
+        }
+        else if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2d.velocity = new Vector2(2, 0);
+            spriteRen.flipX = false;
+            tankAnim.Play("PlayerMoveRight");
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2d.velocity = new Vector2(-2, 0);
+            spriteRen.flipX = true;
+            tankAnim.Play("PlayerMoveRight");
+        }
+    }
+}
---
+++ First pt2/Assets/Scripts/menuManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menuManager : MonoBehaviour
+{
+    // Create public variables(GameObjects)
+    public GameObject player;
+    public GameObject mainMenu;
+    public GameObject curser;
+    public GameObject Level01;
+    public GameObject[] curserPositions;
+    public GameObject playerSelectIndicator;
+    public Text thisText;
+
+    public Camera gameCamera;
+    public Camera mainCamera;
+
+    private bool onStartGameSelection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        onStartGameSelection = false;
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for space or enter key pressed
+        // if pressed then deactivate the menu game object
+        // and show the player character game object
+        //if (Input.GetKey("space") || Input.GetKey("return"))
+        //{
+        //    player.SetActive(true);
+        //    mainMenu.SetActive(false);
+        //}
+
+        // if the user presses up
+        // move curser to next selection
+
+        if(Input.GetKeyDown("up")|| Input.GetKeyDown("down"))
+        {
+            if(onStartGameSelection)
+            {
+                onStartGameSelection = false;
+                thisText = playerSelectIndicator.GetComponent<Text>();
+            }
+            else
+            {
+                 onStartGameSelection = true;
+            }
+           
+        }
+
+        if (onStartGameSelection)
+        {
+            curser.transform.position = curserPositions[1].transform.position;
+        }
+        else
+        {
+            curser.transform.position = curserPositions[0].transform.position;
+        }
+        
+        if(onStartGameSelection &&  Input.GetKeyDown("return"))
+        {
+            Level01.SetActive(true);
+            player.SetActive(true);
+            mainMenu.SetActive(false);
+
+            mainCamera.enabled = false;
+            gameCamera.enabled = true;
+        }
+
+
+    
+        if (onStartGameSelection && Input.GetKeyDown("return"))
+        {
+            // toggle number of players
+        }
+
+        // if user presses down
+        // move curser to next selection
+
+        // if the user presses enter AND if onStartGameSelection is true then
+        // toggle the players
+
+        // if the user presses enter AND if onStartGameSelection is on then
+        // start the game
+    }
+}
